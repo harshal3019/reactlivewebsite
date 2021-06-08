86d18615cfc6a251fd28ef56c032b266055bc7f4
harshal3019 <harshal@example.com>
2021-06-08 12:29:15 +0530

    my first commit reactwebsite

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Covid from './component/Covid';
+
+function App() {
+  return (
+       <>
+       <Covid />
+       </>
+  );
+}
+
+export default App;
---
+++ src/component/Covid.js
@@ -0,0 +1,73 @@
+import React,{useEffect, useState} from 'react'
+import '../component/covid.css';
+const Covid = () => {
+
+    const[data,setUpdate] = useState([]);
+
+       const getCovidData = async() =>
+       {
+         const api = await fetch("https://api.covid19india.org/data.json");
+         const data = await api.json();
+         setUpdate(data.statewise[21]);  
+       }
+    useEffect(() => {
+
+        getCovidData();
+        }, [])
+
+    return (
+        <>
+            <center>
+           <h1>🔴LIVE</h1> 
+           <h2><u>COVID19 CORNA TRACKING APP</u></h2>
+           </center>     
+           <div className="row">
+         <div className="column">
+           <div className="card1">
+            <h2><span>OUR</span>&nbsp;COUNTRY</h2>
+            <h1>INDIA</h1>
+           
+             </div>
+               </div>
+
+            <div className="column">
+             <div className="card2">
+             <h2><span>TOTAL</span>&nbsp;RECOVERED</h2>
+             <h1>{data.recovered}</h1>
+             </div>
+            </div>
+  
+          <div className="column">
+           <div className="card3">
+           <h2><span>TOTAL</span>&nbsp;CONFIRMED</h2>
+             <h1>{data.confirmed}</h1>
+             </div>
+              </div>
+  
+            <div className="column">
+                <div className="card4">
+                <h2><span>TOTAL</span>&nbsp;DEATH</h2>
+                 <h1>{data.deaths}</h1>
+                </div>
+            </div>
+            
+             <div className="column">
+                <div className="card5">
+                <h2><span>TOTAL</span>&nbsp;ACTIVE</h2>
+                 <h1>{data.active}</h1>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card6">
+                <h2><span>LAST</span>&nbsp;UPDATE</h2>
+                 <h1>{data.lastupdatedtime}</h1> 
+   
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Covid
